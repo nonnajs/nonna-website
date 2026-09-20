b7990ebe14b2471d320a6630b086f6418e8210db
SAMANU <manuel.santos@mercedes-benz.com>
2026-09-20 17:37:49 +0100

    adding nonna website

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
   backend "s3" {
     bucket = "nonna-tf-state"
     key    = "nonna-landing.tfstate"
